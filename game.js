c62dad0ca9c62b19d15bbe850cd623b4315a0943
NithishWhitehatjr <81054136+NithishWhitehatjr@users.noreply.github.com>
2021-06-28 12:31:46 -0700

    Add files via upload

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-   
+   function send(){
+       number1= document.getElmentById("number1").value;
+       number2= document.getElmentById("number2").value;
+       actual_answer =parseInt(number1) * parseInt(number2)
+   }
+   question_number = "<h4>"+ number1 + "x" + number2 +"<h4>";
+   input_box= "<br>Answer : <inputtype ='text' id='input_check_box'>";
+   check_button ="<br><br> <button class= 'btn_btn-info' onclick 'check()'>Check</button>";
+   row =question_number + input_box + check_button;
+    document.getElmentById("output").innerHTMl =row;
+        document.getElmentById("number2").value = "";
+       document.getElmentById("number1").value = "";
+       
